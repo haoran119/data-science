095f82ab72971e3c5b22b429d0a66083d6f4f03b
haotang923 <34557994+haotang923@users.noreply.github.com>
2018-08-09 16:39:38 +1000

    Create Adding_a_hover_tool.py

---
+++ src/DataCamp/Interactive Data Visualization with Bokeh/Adding_a_hover_tool.py
@@ -0,0 +1,14 @@
+# Import HoverTool from bokeh.models
+from bokeh.models import HoverTool
+
+# Create a HoverTool: hover
+hover = HoverTool(tooltips=[('Country', '@country')])
+
+# Add the HoverTool to the plot
+plot.add_tools(hover)
+
+# Create layout: layout
+layout = row(widgetbox(slider), plot)
+
+# Add layout to current document
+curdoc().add_root(layout)
